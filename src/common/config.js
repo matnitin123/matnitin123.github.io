d31c037291fa79a160af3f8802f4ad09d81de61d
matnitin123 <matnitin123@gmail.com>
2018-12-31 00:50:18 +0200

    ...

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,4 @@
 module.exports = Object.freeze({
-    API_URL: 'http://www.omdbapi.com',
-    FULL_URL: 'http://www.omdbapi.com/?i=tt3896198&apikey=598339f2'
+    API_URL: 'https://www.omdbapi.com',
+    FULL_URL: 'https://www.omdbapi.com/?i=tt3896198&apikey=598339f2'
 });
